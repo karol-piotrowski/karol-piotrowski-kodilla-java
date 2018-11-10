ba71f2f5f88c5ca9c05b8cdd57a1405bfc94556f
karol-piotrowski <kp0@o2.pl>
2018-11-10 16:53:53 +0100

    Sending task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(15,8)==23) {
+            System.out.println("Calculator.add() method: test OK");
+        } else {
+            System.out.println("Calculator.add() method: Error!");
+        }
+
+        if (calculator.subtract(15,8)==7) {
+            System.out.println("Calculator.subtract() method: test OK");
+        } else {
+            System.out.println("Calculator.subtract() method: Error!");
+        }
     }
 }
