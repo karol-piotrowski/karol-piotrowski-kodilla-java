0478ce191e24edf7db059acd05dfc3787ba9b925
karol-piotrowski <kp0@o2.pl>
2018-11-17 22:02:39 +0100

    Task 6.5 - update 1 - modified conditional expression structure in AdvStatistics class

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
@@ -6,31 +6,24 @@ public class AdvStatistics {
     private int userCount;
     private int postCount;
     private int commentsCount;
-    private double avgPostsPerUser;
-    private double avgCommentsPerUser;
-    private double avgCommentsPerPost;
+    private double avgPostsPerUser = 0;
+    private double avgCommentsPerUser = 0;
+    private double avgCommentsPerPost = 0;
 
     public void calculateAdvStatistics(Statistics statistics) { // divide by 0 values will be calculated as 0
         userCount = statistics.usersNames().size();
         postCount = statistics.postCount();
         commentsCount = statistics.commentsCount();
-        if (userCount == 0 && postCount == 0) {
-            avgPostsPerUser = 0;
-            avgCommentsPerUser = 0;
-            avgCommentsPerPost = 0;
-        } else if (userCount == 0) {
-            avgPostsPerUser = 0;
-            avgCommentsPerUser = 0;
-            avgCommentsPerPost = (double) commentsCount / postCount;
-        } else if (postCount == 0) {
+
+        if (userCount != 0) {
             avgPostsPerUser = (double) postCount / userCount;
             avgCommentsPerUser = (double) commentsCount / userCount;
-            avgCommentsPerPost = 0;
-        } else {
-            avgPostsPerUser = (double) postCount / userCount;
-            avgCommentsPerUser = (double) commentsCount / userCount;
+        }
+
+        if (postCount != 0) {
             avgCommentsPerPost = (double) commentsCount / postCount;
         }
+
     }
 
     public String showStatistics() {
