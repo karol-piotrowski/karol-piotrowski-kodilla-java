9616b3a9d5b8ea194e7a682929e0be2f323653ea
karol-piotrowski <kp0@o2.pl>
2018-11-21 21:41:18 +0100

    Task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -18,5 +19,8 @@ public class StreamMain {
             return resultString;
         }));
         System.out.println(poemBeautifier.beautify(textToDecorate, str -> new StringBuilder(str).reverse().toString()));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.immutable;
+
+public class ForumUser {
+    private final String username;
+    private final String realName;
+
+    public ForumUser(final String username, final String realName) {
+        this.username = username;
+        this.realName = realName;
+    }
+
+    public final String getUsername() {
+        return username;
+    }
+
+    public final String getRealName() {
+        return realName;
+    }
+}
